c267572fa7d24c3bc8bb390d21feefec8abc7a26
Sean Stephenson <seanmichaelstephenson@gmail.com>
2021-04-19 17:18:14 -0700

    more form ctrl

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Typeahead } from 'react-bootstrap-typeahead'; 
 import axios from 'axios';
 import './App.css';
 
@@ -6,13 +7,17 @@ const API = 'http://localhost:3001/api'
 
 function App() {
   const [data, setData] = useState([]);
-  const [selected, setSelected] = useState({});
+  const [selectedProspect, setSelectedProspect] = useState({});
+  const [selectedOption, setSelectedOption] = useState([]);
+  const [disabled, setDisabled] = useState("disabled")
+  const ref = useRef();
 
   async function fetchData() {
     const result = await axios(
       API + '/prospects',
     );
     setData(result.data.data);
+    setDisabled(false)
   }
 
   async function postData(selected) {
@@ -20,38 +25,54 @@ function App() {
       API + '/selected',
       selected.selected
     )
-    setSelected(data)
+    setSelectedProspect(data)
+    setDisabled(false)
+    ref.current.focus()
+    ref.current.clear()
   }
 
   function handleChange(event) {
-    const value = event.target.value;
-    setSelected({selected: {value}})
+    if (event) {
+      setSelectedOption(event)
+      const value = event[0]
+      setSelectedProspect({selected: {value}})
+    }
   }
 
   function handleSubmit(event) {
-    postData(selected)
-    event.preventDefault();
+    setDisabled("disabled")
+    setSelectedOption([])
+    postData(selectedProspect)
+    event.preventDefault()
   }
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const options = data.map((el) => { return el.name });
+
   return (
     <div className="App container">
       <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="prospects-select">Prospects</label>
-          <select className="form-control" id="prospects-select" onChange={handleChange}>
-          <option defaultValue> -- select an option -- </option>
-            {data.map((item) => (
-              <option key={item.name} value={item.name}>{item.name} - {item.position} - {item.school}</option>
-            ))}
-          </select>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <fieldset disabled={disabled}>
+          <div className="form-group">
+            <label htmlFor="prospects" id="prospects-label">Prospects</label>
+            <Typeahead
+              disabled={disabled}
+              id="prospects"
+              labelKey="name"
+              onChange={handleChange}
+              options={options}
+              placeholder="Select"
+              selected={selectedOption}
+              ref={ref}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </fieldset>
         <hr />
-        <pre>{JSON.stringify(selected, null, 2)}</pre>
+        <pre style={{marginTop: '500px'}}>{JSON.stringify(selectedProspect, null, 2)}</pre>
       </form>
     </div>
   );
